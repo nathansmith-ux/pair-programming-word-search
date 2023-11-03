2cdb5111e814b8652088720f0f651cc1b216f63c
Nathan <nathanrmsmith@gmail.com>
2023-11-03 15:00:18 -0400

    Change function to improve it

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    // Check if array is empty
+    if (letters.length === 0) {
+      return false;
     }
-}
+  
+    // Check horizontally
+    for (let row = 0; row < letters.length; row++) {
+      const horizontalWord = letters[row].join('');
+      if (horizontalWord.includes(word)) return true;
+    }
+  
+    // Check vertically
+    for (let col = 0; col < letters[0].length; col++) {
+      let verticalWord = '';
+      for (let row = 0; row < letters.length; row++) {
+        verticalWord += letters[row][col];
 
-module.exports = wordSearch
+        if (verticalWord.length > word.length) {
+          break;
+        }
+        
+        if (verticalWord === word) {
+          return true;
+        }
+      }
+    }
+  
+    return false;
+  };
+  
+  module.exports = wordSearch;
+  
